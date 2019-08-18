b86e648f00bd1a27ea1df8504c6bccc7e32edefe
sswu-kankoo <aha5211@naver.com>
2019-08-18 19:55:12 +0900

    k

---
+++ app/src/main/java/com/kankoo/intenttest/RegistActivity2.java
@@ -0,0 +1,29 @@
+package com.kankoo.intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist2);
+
+        //취소 누르면 메인화면으로 전환
+        Button ChsRegist = (Button) findViewById(R.id.btnCancel);
+        ChsRegist.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(Intent, 100);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/kankoo/intenttest/SubActivity.java
+++ app/src/main/java/com/kankoo/intenttest/SubActivity.java
@@ -1,6 +1,8 @@
 package com.kankoo.intenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,5 +16,38 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        //취소 누르면 메인화면으로 전환
+        Button ChsRegist = (Button) findViewById(R.id.btnCancel);
+        ChsRegist.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(Intent, 100);
+            }
+        });
+
+        //학생 가입화면으로 전환
+        Button ChsRegist1 = (Button) findViewById(R.id.btnRegist1);
+        ChsRegist1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(getApplicationContext(), RegistActivity1.class);
+                startActivityForResult(Intent, 100);
+            }
+        });
+
+        //사업자 가입화면으로 전환
+        Button ChsRegist2 = (Button) findViewById(R.id.btnRegist2);
+        ChsRegist2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(getApplicationContext(), RegistActivity2.class);
+                startActivityForResult(Intent, 100);
+            }
+        });
     }
 }
